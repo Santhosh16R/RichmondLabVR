43a573600a8166e0e71d2169ac77d5d047a8cac9
Santhosh Raghav <103551648+Santhosh16R@users.noreply.github.com>
2026-03-26 18:15:50 +0530

    Update MCQQuestion.cs

--- MCQQuestion.cs
+++ MCQQuestion.cs
@@ -3,13 +3,24 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "MCQ_Question", menuName = "MCQ/Question")]
 public class MCQQuestion : ScriptableObject
 {
-    [TextArea(2, 5)]
+    [TextArea(2,5)]
     public string question;
 
     public string[] options = new string[4];
-
     public int correctAnswerIndex;
 
     [Header("Scoring")]
-    public int marks = 1; // score for this question
+    public int marks = 1;
+
+    [Header("Audio")]
+    public AudioClip questionAudio;
+    public AudioClip correctFeedbackAudio;
+    public AudioClip wrongFeedbackAudio;
+
+    [Header("Text Feedback")]
+    [TextArea(2,5)]
+    public string correctFeedbackText;
+
+    [TextArea(2,5)]
+    public string wrongFeedbackText;
 }
